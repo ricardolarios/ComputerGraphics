c656819858754195d22bb96c7c8ecf4dbb922291
Rikki Larios <larios.r@husky.neu.edu>
2020-01-28 13:55:40 -0500

    Clean up implementation

--- Lab3_Starfield/StarList.cpp
+++ Lab3_Starfield/StarList.cpp
@@ -37,7 +37,6 @@ void StarList::updateAndRender(QImage& image, float delta, const QSize& windowSi
     // Note the conversion to radians
     // TODO: Modify me
     float tanHalfFOV = atan(halfWidth / halfHeight);
-    float sinHalfFOV = asin(halfWidth / halfHypotenuse);
 
     // Iterate through all of your stars 
     for (int i = 0; i < stars_.size(); i++) {
